7f8a0181425c153b7813f81f336ad16d5b694d70
twrblx <95186703+twrblx@users.noreply.github.com>
2021-11-28 14:10:34 -0500

    Create ping.js

---
+++ ping.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "ping",
+    category: "info",
+    description: "Returns latency and API ping",
+  run: async (client, message, args) => {
+           message.channel.send({embed : {
+             description : `> \`\`\`You Latency Is: ${client.ws.ping}ms \`\`\`  `
+           }})
+           .then(msg=>msg.react('📶'))
+           
+           }
+}
+
+
